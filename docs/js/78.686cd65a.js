b07204d8b272d2b11666c4082ba010e7f567caa4
Walter Gandarella <waltergandarella@yesmkt.com>
2023-02-08 11:13:17 +0000

    disable dark mode

---
+++ docs/js/78.686cd65a.js
@@ -0,0 +1,6 @@
+"use strict";(self["webpackChunkyes_cola"]=self["webpackChunkyes_cola"]||[]).push([[78],{6078:(e,t,s)=>{s.r(t),s.d(t,{startStatusTap:()=>a});var n=s(65),o=s(4074),r=s(6587);
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */
+const a=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,n.wj)((()=>{const t=e.innerWidth,s=e.innerHeight,a=document.elementFromPoint(t/2,s/2);if(!a)return;const c=(0,o.a)(a);c&&new Promise((e=>(0,r.c)(c,e))).then((()=>{(0,n.Iu)((async()=>{c.style.setProperty("--overflow","hidden"),await(0,o.s)(c,300),c.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=78.686cd65a.js.map
