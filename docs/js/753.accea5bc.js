475bea792856a55c574ecb05856b7ff424295651
Walter Gandarella <waltergandarella@yesmkt.com>
2023-02-08 11:09:37 +0000

    disable dark mode

--- docs/js/753.accea5bc.js
+++
@@ -1,6 +0,0 @@
-"use strict";(self["webpackChunkyes_cola"]=self["webpackChunkyes_cola"]||[]).push([[753],{4753:(e,n,i)=>{i.r(n),i.d(n,{mdTransitionAnimation:()=>a});var o=i(5980),t=i(5026);
-/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */
-const a=(e,n)=>{var i,a,r;const l="40px",s="0px",c="back"===n.direction,d=n.enteringEl,u=n.leavingEl,m=(0,t.g)(d),b=m.querySelector("ion-toolbar"),f=(0,o.c)();if(f.addElement(m).fill("both").beforeRemoveClass("ion-page-invisible"),c?f.duration((null!==(i=n.duration)&&void 0!==i?i:0)||200).easing("cubic-bezier(0.47,0,0.745,0.715)"):f.duration((null!==(a=n.duration)&&void 0!==a?a:0)||280).easing("cubic-bezier(0.36,0.66,0.04,1)").fromTo("transform",`translateY(${l})`,`translateY(${s})`).fromTo("opacity",.01,1),b){const e=(0,o.c)();e.addElement(b),f.addAnimation(e)}if(u&&c){f.duration((null!==(r=n.duration)&&void 0!==r?r:0)||200).easing("cubic-bezier(0.47,0,0.745,0.715)");const e=(0,o.c)();e.addElement((0,t.g)(u)).onFinish((n=>{1===n&&e.elements.length>0&&e.elements[0].style.setProperty("display","none")})).fromTo("transform",`translateY(${s})`,`translateY(${l})`).fromTo("opacity",1,0),f.addAnimation(e)}return f}}}]);
-//# sourceMappingURL=753.accea5bc.js.map
